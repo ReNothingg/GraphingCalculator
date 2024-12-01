3e140648cf5a14f0c73d83a2801c9b2b1f7d1c77
⊱─≼BLOOTBER≽─⊰ <pashasob2009@gmail.com>
2024-12-01 16:38:25 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Mathway
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.plotButton = new System.Windows.Forms.Button();
+            this.sliderALabel = new System.Windows.Forms.Label();
+            this.sliderBLabel = new System.Windows.Forms.Label();
+            this.plotView = new OxyPlot.WindowsForms.PlotView();
+            this.sliderA = new System.Windows.Forms.HScrollBar();
+            this.sliderB = new System.Windows.Forms.HScrollBar();
+            this.cofA = new System.Windows.Forms.Label();
+            this.cofB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.Location = new System.Drawing.Point(10, 26);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(360, 51);
+            this.inputTextBox.TabIndex = 1;
+            this.inputTextBox.Text = "y=2x";
+            // 
+            // plotButton
+            // 
+            this.plotButton.BackColor = System.Drawing.Color.IndianRed;
+            this.plotButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plotButton.Location = new System.Drawing.Point(10, 470);
+            this.plotButton.Name = "plotButton";
+            this.plotButton.Size = new System.Drawing.Size(360, 47);
+            this.plotButton.TabIndex = 2;
+            this.plotButton.Text = "Построить график";
+            this.plotButton.UseVisualStyleBackColor = false;
+            this.plotButton.Click += new System.EventHandler(this.PlotButton_Click);
+            // 
+            // sliderALabel
+            // 
+            this.sliderALabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sliderALabel.Location = new System.Drawing.Point(10, 366);
+            this.sliderALabel.Name = "sliderALabel";
+            this.sliderALabel.Size = new System.Drawing.Size(100, 33);
+            this.sliderALabel.TabIndex = 3;
+            this.sliderALabel.Text = "Коэффициент A:";
+            // 
+            // sliderBLabel
+            // 
+            this.sliderBLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sliderBLabel.Location = new System.Drawing.Point(10, 438);
+            this.sliderBLabel.Name = "sliderBLabel";
+            this.sliderBLabel.Size = new System.Drawing.Size(100, 29);
+            this.sliderBLabel.TabIndex = 5;
+            this.sliderBLabel.Text = "Коэффициент B:";
+            // 
+            // plotView
+            // 
+            this.plotView.BackColor = System.Drawing.Color.Silver;
+            this.plotView.Location = new System.Drawing.Point(376, 10);
+            this.plotView.Name = "plotView";
+            this.plotView.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView.Size = new System.Drawing.Size(622, 507);
+            this.plotView.TabIndex = 7;
+            this.plotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // sliderA
+            // 
+            this.sliderA.Location = new System.Drawing.Point(113, 366);
+            this.sliderA.Name = "sliderA";
+            this.sliderA.Size = new System.Drawing.Size(218, 33);
+            this.sliderA.TabIndex = 8;
+            this.sliderA.Value = 1;
+            this.sliderA.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sliderA_Scroll);
+            // 
+            // sliderB
+            // 
+            this.sliderB.Location = new System.Drawing.Point(113, 438);
+            this.sliderB.Name = "sliderB";
+            this.sliderB.Size = new System.Drawing.Size(218, 29);
+            this.sliderB.TabIndex = 9;
+            this.sliderB.Value = 1;
+            this.sliderB.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sliderB_Scroll);
+            // 
+            // cofA
+            // 
+            this.cofA.AutoSize = true;
+            this.cofA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cofA.Location = new System.Drawing.Point(334, 366);
+            this.cofA.Name = "cofA";
+            this.cofA.Size = new System.Drawing.Size(23, 25);
+            this.cofA.TabIndex = 10;
+            this.cofA.Text = "1";
+            // 
+            // cofB
+            // 
+            this.cofB.AutoSize = true;
+            this.cofB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cofB.Location = new System.Drawing.Point(334, 442);
+            this.cofB.Name = "cofB";
+            this.cofB.Size = new System.Drawing.Size(23, 25);
+            this.cofB.TabIndex = 11;
+            this.cofB.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1010, 524);
+            this.Controls.Add(this.cofB);
+            this.Controls.Add(this.cofA);
+            this.Controls.Add(this.sliderB);
+            this.Controls.Add(this.sliderA);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.plotButton);
+            this.Controls.Add(this.sliderALabel);
+            this.Controls.Add(this.sliderBLabel);
+            this.Controls.Add(this.plotView);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Mathway - Построение графиков";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button plotButton;
+        private System.Windows.Forms.Label sliderALabel;
+        private System.Windows.Forms.Label sliderBLabel;
+        private OxyPlot.WindowsForms.PlotView plotView;
+        private System.Windows.Forms.HScrollBar sliderA;
+        private System.Windows.Forms.HScrollBar sliderB;
+        private System.Windows.Forms.Label cofA;
+        private System.Windows.Forms.Label cofB;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System.Globalization;
+
+namespace Mathway
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlotButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string input = inputTextBox.Text.ToLower().Replace(" ", "");
+
+                if (!input.Contains("x"))
+                {
+                    MessageBox.Show("Функция должна содержать переменную x.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!input.StartsWith("y="))
+                {
+                    MessageBox.Show("Функция должна начинаться с 'y='.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                input = input.Substring(2);
+
+                var model = new PlotModel { Title = "График функции" };
+                var series = new LineSeries { Color = OxyColors.Blue };
+
+                double a = (double)sliderA.Value;
+                double b = (double)sliderB.Value;
+
+
+                for (double x = -10; x <= 10; x += 0.1)
+                {
+                    double y = EvaluateFunction(input, x, a, b);
+                    if (double.IsNaN(y) || double.IsInfinity(y))
+                        continue;
+                    series.Points.Add(new DataPoint(x, y));
+                }
+
+                model.Series.Add(series);
+                plotView.Model = model;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка построения графика: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private double EvaluateFunction(string expression, double x, double a, double b)
+        {
+            try
+            {
+                expression = expression.Replace("sin", "Math.Sin")
+                                       .Replace("cos", "Math.Cos")
+                                       .Replace("tan", "Math.Tan")
+                                       .Replace("exp", "Math.Exp")
+                                       .Replace("log", "Math.Log")
+                                       .Replace("sqrt", "Math.Sqrt");
+
+                expression = expression.Replace("x", x.ToString(CultureInfo.InvariantCulture))
+                                       .Replace("a", a.ToString(CultureInfo.InvariantCulture))
+                                       .Replace("b", b.ToString(CultureInfo.InvariantCulture));
+
+                return ExecuteManualEvaluation(expression);
+            }
+            catch
+            {
+                throw new Exception("Некорректное выражение. Проверьте ввод.");
+            }
+        }
+
+        private double ExecuteManualEvaluation(string expression)
+        {
+            try
+            {
+                var table = new System.Data.DataTable();
+                table.Locale = CultureInfo.InvariantCulture;
+                var result = table.Compute(expression, null);
+                return Convert.ToDouble(result);
+            }
+            catch
+            {
+                throw new Exception("Некорректное выражение. Проверьте ввод.");
+            }
+        }
+
+        private void sliderA_Scroll(object sender, ScrollEventArgs e) => cofA.Text = Convert.ToString((double)sliderA.Value);
+
+        private void sliderB_Scroll(object sender, ScrollEventArgs e) => cofB.Text = Convert.ToString((double)sliderB.Value);
+    }
+}
